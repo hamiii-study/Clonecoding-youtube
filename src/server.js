cb6cad5c5f571c5e810a73e89017de4c3caa2969
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-10 01:21:38 +0900

    server basic1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,28 @@
-import express from "express";
-//const express = require("express");
-// const app = express();
+import express from "express"; //node_modules에서 찾아줌!!
 
-console.log("Haaa!!!");
+const PORT = 5000;
+
+const app = express(); //server = application 만들기
+//server is always conneted with internet
+//type google.com -> request, server is always listening
+
+//application setting
+
+app.get("/", (req, res) => {
+  console.log(req);
+  //return res.end, return res.send("hi!!!!")
+}); // route, callback
+
+const handleLogin = (req, res) => {
+  return res.send("Login here!!");
+};
+app.get("/login", handleLogin);
+//application listening
+const handelListening = () =>
+  console.log(`✔ Server listenting on port http://localhost:${PORT} 🎉`);
+app.listen(PORT, handelListening); //what port do server listen?, callback
+
+//HTTP -> How server communicate with us
+//GET -> Http method
+
+//browser-> request page to the server, and get page for us
